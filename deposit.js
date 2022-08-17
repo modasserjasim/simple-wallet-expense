964cd6c62715526665b5744a0c7b3d9f17eaad9f
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-08-17 20:19:48 +0600

    validation complete

--- deposit.js
+++ deposit.js
@@ -2,22 +2,25 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-field');
     const depositAmountString = depositField.value;
     const depositAmount = parseFloat(depositAmountString);
-    console.log(depositAmount)
 
-    const depositCurrentAmount = document.getElementById('deposit-amount');
-    const depositCurrentAmountString = depositCurrentAmount.innerText;
-    const depositTotalAmount = parseFloat(depositCurrentAmountString);
+    if(depositAmount > 0 || depositAmount == NaN){
+        const depositCurrentAmount = document.getElementById('deposit-amount');
+        const depositCurrentAmountString = depositCurrentAmount.innerText;
+        const depositCurrentTotalAmount = parseFloat(depositCurrentAmountString);
 
-    const depositTotal = depositAmount + depositTotalAmount;
-    depositCurrentAmount.innerText = depositTotal;
+        const depositTotal = depositAmount + depositCurrentTotalAmount;
+        depositCurrentAmount.innerText = depositTotal;
 
-    //add balance amount
-    const balanceCurrentAmount = document.getElementById('balance-amount');
-    const balanceCurrentAmountString = balanceCurrentAmount.innerText;
-    const balanceTotalAmount = parseFloat(balanceCurrentAmountString);
-    
-    const balanceTotal = balanceTotalAmount + depositAmount;
-    balanceCurrentAmount.innerText = balanceTotal;
+        //add balance amount
+        const balanceCurrentAmount = document.getElementById('balance-amount');
+        const balanceCurrentAmountString = balanceCurrentAmount.innerText;
+        const balanceTotalAmount = parseFloat(balanceCurrentAmountString);
+        
+        const balanceTotal = balanceTotalAmount + depositAmount;
+        balanceCurrentAmount.innerText = balanceTotal;
+    } else{
+        alert('Please enter a valid integer number');
+    }
 
     depositField.value = '';
     
--- withdrawal.js
+++ withdrawal.js
@@ -3,23 +3,33 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
     const withdrawAmountString = withdrawField.value;
     const withdrawalInputAmount = parseFloat(withdrawAmountString);
 
-    const withdrawText = document.getElementById('withdraw-amount');
-    const withdrawCurrentAmount = withdrawText.innerText;
-    const withdrawCurrentAmountString = parseFloat(withdrawCurrentAmount);
-    const withdrawCurrentTotal = withdrawCurrentAmountString;
+    if(withdrawalInputAmount > 0 || withdrawalInputAmount == NaN){
+        const withdrawText = document.getElementById('withdraw-amount');
+        const withdrawCurrentAmount = withdrawText.innerText;
+        const withdrawCurrentAmountString = parseFloat(withdrawCurrentAmount);
+        const withdrawCurrentTotal = withdrawCurrentAmountString;
 
-    const withdrawalTotal = withdrawalInputAmount + withdrawCurrentTotal;
-    withdrawText.innerText = withdrawalTotal;
-
-    // remove from the main balance
 
        //add balance amount
        const balanceCurrentAmount = document.getElementById('balance-amount');
        const balanceCurrentAmountString = balanceCurrentAmount.innerText;
        const balanceTotalAmount = parseFloat(balanceCurrentAmountString);
+
+       if(withdrawalInputAmount > balanceTotalAmount){
+         alert("You don't have sufficient balance");
+         withdrawField.value = '';
+         return;
+       }
+        // remove from the main balance
+       const withdrawalTotal = withdrawalInputAmount + withdrawCurrentTotal;
+       withdrawText.innerText = withdrawalTotal;
        
        const balanceTotal = balanceTotalAmount - withdrawalInputAmount;
        balanceCurrentAmount.innerText = balanceTotal;
-   
        withdrawField.value = '';
+    } else{
+        alert('Please input a valid number')
+    }
+   
+       
 })
